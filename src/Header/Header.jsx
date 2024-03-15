492b142d3bcb493774a455d80ee77e7a0016a3d5
EMON <mamudmdemon@gmail.com>
2024-03-15 22:53:33 +0600

    navbar completed without responsive

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,4 +1,4 @@
-
+import profile from '../assets/Images/user.png'
 const Header = () => {
   return (
     <div>
@@ -8,40 +8,33 @@ const Header = () => {
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 text-[#150B2BB2] text-base shadow bg-base-100 rounded-box w-52">
+        <li><a>Home</a></li>
+        <li><a>Recipes</a></li>
+        <li><a>About</a></li>
+        <li><a>Search</a></li>
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="text-[#150B2B] text-4xl font-bold">Tasty Twist</a>
   </div>
   <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+    <ul className="menu menu-horizontal px-1 text-base text-[#150B2BB2]">
+      <li><a>Home</a></li>
+      <li><a>Recipes</a></li>
+      <li><a>About</a></li>
+      <li><a>Search</a></li>
     </ul>
   </div>
-  <div className="form-control">
-      <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+  <div className="navbar-end">
+    <div className=" input input-bordered rounded-3xl flex items-center gap-2">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+    <input type="text" className="grow " placeholder="Search" />
+    </div>
+     <div className= ' ml-3 bg-[#0BE58A] rounded-3xl w-12 h-12 flex items-center justify-center'><button className=' rounded-full w-8 '><img src={profile} alt="" /></button></div>
     </div>
 </div>
-    </div>
+    
+</div>
   );
 };
 
