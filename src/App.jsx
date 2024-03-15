80d825f5bfd2b4d2fba31b31122aae5ea6f7aa75
EMON <mamudmdemon@gmail.com>
2024-03-15 19:39:24 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Header from './Header/Header'
+
+function App() {
+
+  return (
+    <div className='max-w-7xl mx-auto mt-8'>
+      <Header></Header>
+    </div>
+      
+    
+  )
+}
+
+export default App
